dc46bb05a71fdb2e63992672a8ccc0685d1f87bc
SanjokMangrati <smangrati25@gmail.com>
2024-12-07 19:42:20 +0530

    added theming, default to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "TRENDORA",
@@ -16,7 +17,14 @@ export default function RootLayout({
       <body
         className="antialiased"
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
